5e3eed527bb0a587c0e78bff2e47f728cd1ce17f
Horacio Mota <horaciomota93@gmail.com>
2023-11-21 22:36:21 +0000

    first commit

---
+++ TabNews/TabNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabNewsApp.swift
+//  TabNews
+//
+//  Created by Horacio Mota on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TabNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
